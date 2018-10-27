e2b6a2c06cd5cb6bbdfe78ae3e03eb2c78d2f097
Soumya Ranjan Dash <soumyardash27@gmail.com>
2018-10-27 17:46:44 +0530

    some changes

--- c/fizzbuzz.c
+++ c/fizzbuzz.c
@@ -2,23 +2,23 @@
 #include <string.h>
 
 int main(void) {
-    char output[5];
     int flag=0;
     int i;
 	for(int number=0;number<100;number++){
 	    flag=0;
 	    i=number+1;
-	    strcpy(output,"");
-	    if(i%3==0)
-	        strcpy(output,"Fizz");
-	    if(i%5==0)
-	        strcpy(output,"Buzz");
-	    if(strcmp(output,"")==0)
+	    if(i%3==0){
+	        printf("Fizz ");
 	        flag=1;
-	    if(flag==1)
+	    }
+	    if(i%5==0){
+	        printf("Buzz ");
+	        flag=1;
+	    }
+	    if(flag==0)
 	        printf("%d\n",i);
 	    else
-	        printf("%s\n",output);
+	        printf("\n");
 	}
 	return 0;
 }
